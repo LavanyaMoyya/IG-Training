2040e899c5b82de41acce0926b5d37e12ce2ec0d
LavanyaMoyya <47386515+LavanyaMoyya@users.noreply.github.com>
2022-09-02 09:47:16 +0530

    Add files via upload

---
+++ Spring-Batch-Processing/src/main/java/com/batch/springbatchprocessing/config/CustomerProcessor.java
@@ -0,0 +1,11 @@
+package com.batch.springbatchprocessing.config;
+
+import com.batch.springbatchprocessing.entity.Customer;
+import org.springframework.batch.item.ItemProcessor;
+
+public class CustomerProcessor implements ItemProcessor<Customer,Customer> {
+    @Override
+    public Customer process(Customer customer) throws Exception {
+        return customer;
+    }
+}
